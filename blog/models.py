71d77a7e27cd2e2296a801f4d143a7b58d0b54b0
andres <desarrollolibre.net@gmail.com>
2025-07-13 06:23:07 -0400

    migraciones

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
+from django.contrib.contenttypes.fields import GenericRelation, GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
 from django.db import models
 
 # Create your models here.
@@ -9,3 +12,54 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+
+class Post(models.Model):
+
+    LANGUAGE_CHOICES = [
+        ('english', 'English'),
+        ('spanish', 'Spanish'),
+        ]
+    
+    POSTED_CHOICES = [
+        ('yes', 'Yes'),
+        ('not', 'Not'),
+        ]
+
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    date = models.DateField(auto_now_add=True)
+    path = models.CharField(max_length=255, null=True, blank=True)
+    image = models.FileField(upload_to='posts/', null=True, blank=True)
+    content = models.CharField(max_length=500)
+
+    posted = models.CharField(max_length=3, choices=POSTED_CHOICES, default='not')
+    language = models.CharField(max_length=10, choices=LANGUAGE_CHOICES, default='spanish')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
+
+    # images = GenericRelation('djangoshopping.gallery.Image')
+    # images = GenericRelation('store.Image')
+    tags = GenericRelation('Taggable')
+
+    def __str__(self):
+        return self.title
+    
+
+    
+
+# TAGS
+class Tag(models.Model):
+    title = models.CharField(max_length=500)
+    slug = models.SlugField(max_length=500)
+
+    def __str__(self):
+        return self.title
+
+    
+class Taggable(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='taggables')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    class Meta:
+        unique_together = ('tag', 'content_type', 'object_id')
--- store/models.py
+++ store/models.py
@@ -1,3 +1,70 @@
+from django.contrib.contenttypes.fields import GenericRelation, GenericForeignKey
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+
 from django.db import models
 
+from blog.models import Post
+
 # Create your models here.
+
+class Book(models.Model):
+    
+    POSTED_CHOICES = [
+        ('yes', 'Yes'),
+        ('not', 'Not'),
+        ]
+
+    title = models.CharField(max_length=250, unique=True)
+    subtitle = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    date = models.DateField(auto_now_add=True)
+    path = models.CharField(max_length=255, null=True, blank=True)
+    image = models.CharField(max_length=255, null=True, blank=True)
+    content = models.CharField(max_length=500)
+    posted = models.CharField(max_length=3, choices=POSTED_CHOICES, default='not')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='books')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    page = models.PositiveIntegerField(default=0)
+
+    price = models.DecimalField(max_digits=7, decimal_places=2, default=0.00)
+    price_offert = models.DecimalField(max_digits=7, decimal_places=2, default=0.00)
+
+    images = GenericRelation('Image')
+    tags = GenericRelation('blog.Taggable')
+
+    def __str__(self):
+        return self.title
+
+class Image(models.Model):
+    path = models.CharField(max_length=255)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+class Payment(models.Model):
+    
+    PAYMENT_CHOICES = [
+        ('paypal', 'PayPal'),
+        ('stripe', 'Stripe'),
+        ]
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    posted = models.CharField(max_length=10, choices=PAYMENT_CHOICES, default='paypal')
+    coupon = models.CharField(max_length=100, null=True, blank=True)
+    orderId = models.CharField(max_length=500, unique=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2, default=0.00)
+    trace = models.CharField(max_length=2000)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    path = models.CharField(max_length=255, null=True, blank=True)
+    image = models.CharField(max_length=255, null=True, blank=True)
+    content = models.CharField(max_length=500)
+    
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    paymentable = GenericForeignKey('content_type', 'object_id')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.orderId}"
