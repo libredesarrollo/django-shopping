2d3b5eed35acd2e62dffd96789f3fc9cd6696575
andres <desarrollolibre.net@gmail.com>
2025-07-25 13:18:28 -0400

    varios

--- djangoshopping/settings.py
+++ djangoshopping/settings.py
@@ -51,8 +51,7 @@ INSTALLED_APPS = [
 #Tailwind
 TAILWIND_APP_NAME = 'theme'
 
-
-# NPM_BIN_PATH = os.environ.get('NPM_BIN_PATH', None)
+NPM_BIN_PATH = os.environ.get('NPM_BIN_PATH', 'npm')
 # if platform.system() == "Windows":
 #     NPM_BIN_PATH = "C:/Users/andre/.config/herd/bin/nvm/v23.11.0/npm.cmd"
 
