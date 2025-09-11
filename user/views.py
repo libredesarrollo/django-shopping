1a6687e66d0ae2a200bc8974dd70df6c0ff2911a
andres <desarrollolibre.net@gmail.com>
2025-09-11 07:59:44 -0400

    varios

--- user/views.py
+++ user/views.py
@@ -1,11 +1,15 @@
+import logging
 from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
 
+logger = logging.getLogger(__name__)
+
 class ProfileView(TemplateView):
     template_name = 'account/profile.html'
 
     def get_context_data(self, **kwargs):
+        logger.error(f"Test Log:", exc_info=True)
         context = super().get_context_data(**kwargs)
         context['lang_code'] = 'en'
