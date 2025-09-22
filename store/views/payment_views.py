5a8597ca95499a6a9ff37b19504b25854e3e8cf8
andres <desarrollolibre.net@gmail.com>
2025-09-22 14:22:18 -0400

    varios

--- store/views/payment_views.py
+++ store/views/payment_views.py
@@ -14,7 +14,11 @@ from django.urls import reverse
 from ..models import Book, Product, Payment
 from ..utils.payment import BasePayment
 
+from ..utils.coupon import UtilityCoupon
+
 from django.contrib.contenttypes.models import ContentType
+
+import json
 
 # listado de pagos del usuario        
 class UserPaymentsView(LoginRequiredMixin, ListView):
@@ -57,7 +61,22 @@ class BasePaymentView(LoginRequiredMixin, View, BasePayment):
             order_id = request.GET.get('order_id', '')
             if not order_id:
                 return self._redirect_or_json(request, "s.payment.error", message_error=_("Not Order Found"))
-     
+            
+        # busca si ha cupon
+        # por get viene de stripe
+        coupon = request.GET.get('coupon')
+        if not coupon:
+            # por post viene de paypal
+            coupon = request.POST.get('coupon') # NO HACE falta, ya que por paypal espera es application/json
+
+        # verificamos si la peticion es de tipo json (paypal)
+        if not coupon and request.content_type == 'application/json':
+            try:
+                body = json.loads(request.body)
+                coupon = body.get('coupon')
+            except (json.JSONDecodeError, AttributeError):
+                coupon = None
+
         # procesamos la orden
         response = self.process_order(order_id, type)
         if response is False:
@@ -74,7 +93,7 @@ class BasePaymentView(LoginRequiredMixin, View, BasePayment):
         payment = Payment.objects.create(
             user=user,
             type=self.type,
-            coupon=None,
+            coupon=coupon,
             orderId=order_id,
             price=self.price,
             trace=self.trace,
@@ -95,25 +114,47 @@ class PaymentProductView(BasePaymentView):
     url_kwarg = "product_id"
 
 # STRIPE Helper View
-class StripeView(LoginRequiredMixin, View, BasePayment):
+class StripeView(LoginRequiredMixin, View, BasePayment, UtilityCoupon):
     def post(self, request):
         entity = request.POST.get('entity', '')
         id = request.POST.get('id', '')
+        coupon = request.POST.get('coupon', '')
         payment_url = request.POST.get('url', '')
         
+        # *** buscamos la entidad
         if entity == 'book':
             try:
                 product = Book.objects.get(id=id)
             except Book.DoesNotExist:
                 return JsonResponse({'error': _("Not Book Found")}, status=404)
             
+        if entity == 'product':
+            try:
+                product = Product.objects.get(id=id)
+            except Product.DoesNotExist:
+                return JsonResponse({'error': _("Not Product Found")}, status=404)
+            
         # ruta relativa
         relative_url = reverse('s.payment.cancel')
 
         # Ruta absoluta
         cancel_absolute_url = request.build_absolute_uri(relative_url)
+
+        # *** verificamos si hay un cupon como parte de la URL
+        if coupon:
+             # verificamos el cupon
+            messageCoupon = self.check_coupon(coupon, product.price_offert)
+            if messageCoupon.get('status') == 'success':
+                # es valido, aplica el cupon
+                product.price_offert = self.final_price 
+                # aplicamos el cupon en la URL
+                payment_url = (f"{payment_url}?coupon={coupon}")+"&order_id={CHECKOUT_SESSION_ID}"
+        else:
+            # Generamos la URL de pago por defecto
+            payment_url = payment_url +'?order_id={CHECKOUT_SESSION_ID}'
+  
             
-        return JsonResponse({'id': self.generate_session_id(product.title, product.price, payment_url +'?order_id={CHECKOUT_SESSION_ID}', cancel_absolute_url)})
+        return JsonResponse({'id': self.generate_session_id(product.title, product.price_offert, payment_url, cancel_absolute_url)})
 
 #*** Pantallas de pago exito, error, cancelado    
 class PaymentSuccessView(LoginRequiredMixin, View):
--- store/views/product_views.py
+++ store/views/product_views.py
@@ -97,22 +97,34 @@ class ProductShow(DetailView, UtilityCoupon):
         context = super().get_context_data(**kwargs)
         product = self.object  # Obtienes la instancia del producto
         
+        # claves de pago
         context['paypal_client_id'] = settings.PAYPAL_CLIENT_ID     
         context['stripe_key'] = settings.STRIPE_KEY     
+        # template de detalle personalizado
         context["template_path"] = f"store/product/partials/detail/{product.product_type.slug}.html"
+
+        # indica si es el paso 1 (cupon y boton de pago) o 2 (paypal y stripe botones)
         context["step_one"] = self.step_one 
 
+        # si hay cupon, lo procesa
         if self.coupon:
+            # verifica si es valido
             self.messageCoupon = self.check_coupon(self.coupon, product.price_offert)
             context["coupon"] = self.coupon
             
             if self.messageCoupon.get('status') == 'success':
-                product.price_offert = f"{self.final_price:.2f}"   # Para evitar errores en 'es' al definir flotantes con ,
+                # product.price_offert = f"{self.final_price:.2f}"   # Para evitar errores en 'es' al definir flotantes con ,
+                # es valido, aplica el cupon
+                product.price_offert = self.final_price 
             else:
+                # no es valido, sigue en el paso 1
                 context["step_one"] = None
-                product.price_offert = f"{product.price_offert:.2f}"   # Para evitar errores en 'es' al definir flotantes con ,
         
             context["messageCoupon"] = self.messageCoupon   
+
+        # Para evitar errores en 'es' al definir flotantes con ,
+        product.price_offert = f"{product.price_offert:.2f}"
+            
         
    
 # 1 cupon esta establecido correcto!
